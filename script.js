3b967c096f6dad8d30de189575f3e8342b82ec87
stephensam02 <143948910+stephensam02@users.noreply.github.com>
2023-12-18 11:07:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Add your login logic here
+    // For demonstration purposes, just show an alert with entered credentials
+    //alert("Username: " + username + "\nPassword: " + password);
+}
